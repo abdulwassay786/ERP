6d65a8fead3e6022f9d18836162f562bfc0dfac4
abdulwassay786 <abdulwassayansari786@gmail.com>
2026-02-09 13:32:09 +0500

    prod setting

--- erp_project/settings.py
+++ erp_project/settings.py
@@ -9,23 +9,28 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# Load it
+load_dotenv()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@22xt97jvgo0vkj9cl_y*9ct2ef%@)9dfj^^m$c#asm@anv721'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['erp_project.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
