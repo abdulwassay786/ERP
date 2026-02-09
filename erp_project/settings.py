47f0b286d546fb56396b0f54dbb51d2923bf775c
abdulwassay786 <abdulwassayansari786@gmail.com>
2026-02-09 13:47:40 +0500

    updated AllowedHosts

--- erp_project/settings.py
+++ erp_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['erp_project.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['erp_project.onrender.com', 'localhost', '127.0.0.1', 'erp-21gn.onrender.com']
 
 
 # Application definition
